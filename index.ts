7a49f3b07c80dd9719f0f178648b299871da9583
v.khvostov <v.khvostov@tinkoff.ru>
2019-08-20 10:47:44 +0300

    prepare to publish

--- index.ts
+++ index.ts
@@ -9,6 +9,9 @@ export default async (login: string, pass: string) => {
     const emailInput = await page.$("[placeholder=Email]");
     const passwordInput = await page.$("[placeholder=Password]");
     const submitButton = await page.$('[value="Login"]');
+    if (!emailInput || !passwordInput || !submitButton) {
+        throw new Error('No found on of requred DOM elem, maybe quora change HTML layout :(')
+    }
     await emailInput.type(login);
     await passwordInput.type(pass);
     await submitButton.click();
@@ -17,12 +20,21 @@ export default async (login: string, pass: string) => {
     await page.waitForNavigation();
 
     const firstTitleNode = await page.$('.ui_content_title');
+    if (!firstTitleNode) {
+        throw new Error('No found tittle, maybe quora change HTML layout :(')
+    }
     const firstTitleText = await (await firstTitleNode.getProperty('textContent')).jsonValue();
 
     const firstMoreButton = await page.$('.ui_qtext_more_link');
+    if (!firstMoreButton) {
+        throw new Error('No found More button, maybe quora change HTML layout :(')
+    }
     await firstMoreButton.click();
     await page.waitFor(300);
     const contentSectionNode = await page.$('.ExpandedAnswer');
+    if(!contentSectionNode) {
+        throw new Error('No found content block, maybe quora change HTML layout :(')
+    }
     const contentText = await (await contentSectionNode.getProperty('textContent')).jsonValue();
 
     await browser.close();
